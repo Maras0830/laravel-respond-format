3d9e130ac3f9b3a58b6ddbd2c697d7b8b238c5ee
Maras Chen <maraschen@4gamers.com.tw>
2017-06-14 13:50:58 +0800

    hotfix response use

--- src/respond.php
+++ src/respond.php
@@ -1,6 +1,5 @@
 <?php
 
-use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Response;
 
 CONST SUCCESS_STATUS = 'success';
@@ -10,12 +9,12 @@ CONST MEMBER_NOT_FOUND = 'member_not_found';
 CONST INPUT_IS_REQUIRED = 'input_required';
 CONST INVALID_FORM_DATA = 'invalid_form_data';
 
-CONST SUCCESS_CODE = Response::HTTP_OK; // 200
-CONST NOT_FOUND_CODE = Response::HTTP_NOT_FOUND; // 404
-CONST TOKEN_REQUIRED_CODE = Response::HTTP_NON_AUTHORITATIVE_INFORMATION; // 203
-CONST MEMBER_NOT_FOUND_CODE = Response::HTTP_NOT_FOUND; // 404
-CONST INPUT_IS_REQUIRED_CODE = Response::HTTP_UNPROCESSABLE_ENTITY; // 422
-CONST INVALID_FORM_DATA_CODE = Response::HTTP_UNPROCESSABLE_ENTITY; // 422
+CONST SUCCESS_CODE = Illuminate\Http\Response::HTTP_OK; // 200
+CONST NOT_FOUND_CODE = Illuminate\Http\Response::HTTP_NOT_FOUND; // 404
+CONST TOKEN_REQUIRED_CODE = Illuminate\Http\Response::HTTP_NON_AUTHORITATIVE_INFORMATION; // 203
+CONST MEMBER_NOT_FOUND_CODE = Illuminate\Http\Response::HTTP_NOT_FOUND; // 404
+CONST INPUT_IS_REQUIRED_CODE = Illuminate\Http\Response::HTTP_UNPROCESSABLE_ENTITY; // 422
+CONST INVALID_FORM_DATA_CODE = Illuminate\Http\Response::HTTP_UNPROCESSABLE_ENTITY; // 422
 
 if (! function_exists('respond')) {
     function respond($data, array $error = [], ...$meta)
@@ -31,15 +30,15 @@ if (! function_exists('respond')) {
         if (!empty($meta))
             $response['meta'] = array_collapse($meta);
 
-        return new JsonResponse($response,200);
+        return new Illuminate\Http\JsonResponse($response,200);
     }
 }
 
 if (! function_exists('not_found')) {
     /**
-     * @return JsonResponse
+     * @return Illuminate\Http\JsonResponse
      */
-    function not_found(): JsonResponse
+    function not_found(): Illuminate\Http\JsonResponse
     {
         return respond([], error_format('Data not found.', NOT_FOUND_CODE, NOT_FOUND_STATUS));
     }
@@ -63,9 +62,9 @@ if (! function_exists('error_format')) {
 if (! function_exists('input_is_required')) {
     /**
      * @param $data
-     * @return JsonResponse
+     * @return Illuminate\Http\JsonResponse
      */
-    function input_is_required($data = []): JsonResponse
+    function input_is_required($data = []): Illuminate\Http\JsonResponse
     {
         return respond([], error_format('Input required.', INPUT_IS_REQUIRED_CODE, INPUT_IS_REQUIRED, $data));
     }
@@ -74,9 +73,9 @@ if (! function_exists('input_is_required')) {
 
 if (! function_exists('token_required')) {
     /**
-     * @return JsonResponse
+     * @return Illuminate\Http\JsonResponse
      */
-    function token_required(): JsonResponse
+    function token_required(): Illuminate\Http\JsonResponse
     {
         return respond([], error_format('Token required.', TOKEN_REQUIRED_CODE, TOKEN_REQUIRED));
     }
@@ -85,9 +84,9 @@ if (! function_exists('token_required')) {
 
 if (! function_exists('member_not_found')) {
     /**
-     * @return JsonResponse
+     * @return Illuminate\Http\JsonResponse
      */
-    function member_not_found(): JsonResponse
+    function member_not_found(): Illuminate\Http\JsonResponse
     {
         return respond([], error_format('Member not found.', MEMBER_NOT_FOUND_CODE, MEMBER_NOT_FOUND));
     }
@@ -96,9 +95,9 @@ if (! function_exists('member_not_found')) {
 if (! function_exists('form_data_invalid')) {
     /**
      * @param array $data
-     * @return JsonResponse
+     * @return Illuminate\Http\JsonResponse
      */
-    function form_data_invalid($data = []): JsonResponse
+    function form_data_invalid($data = []): Illuminate\Http\JsonResponse
     {
         return respond([], error_format('Form data Invalid.', INVALID_FORM_DATA_CODE, INVALID_FORM_DATA, $data));
     }
